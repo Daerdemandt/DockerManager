3242e9dfcf930dbf42fc28642f2c737b8b6c5716
Roman Kuzmichev <daerdemandt@gmail.com>
2015-06-10 18:10:14 +0000

    When target is removed, targets that depend on it are removed too. Removing targets is now operational.

--- remove.sh
+++ remove.sh
@@ -3,8 +3,6 @@
 # Used to remove targets
 # Expects one argument - target to remove
 
-# TODO: also remove corresponding images
-
 # include some functions
 . "$(dirname $0)/shared_code.sh"
 
@@ -21,15 +19,22 @@ if ! target_exists $TARGET_DIR_NAME_FULL ; then
 	exit 1;
 fi
 
+# Before removing our target, let's remove targets that depend on it
+for child in $(get_dependants $TARGET_DIR_NAME_FULL); do
+	# remove slash from beginning if any
+	CHILD_TARGET_NAME=$(echo "$TARGET/$child" | sed 's/^\///' )
+	$0 $CHILD_TARGET_NAME
+done
+
 rm -rf $TARGET_DIR_NAME_FULL
 
-UNTESTED_ID=$(docker images | grep "$(untested_image_name $TARGET)" | awk '{print $3}')
-if [[ -n $UNTESTED_ID ]] ; then
+UNTESTED_ID=$(docker images | grep "$(basic_image_name $TARGET)\s*tested" | awk '{print $3}')
+if [ $UNTESTED_ID ] ; then
 	docker rmi -f "$UNTESTED_ID"
 fi
 
-TESTED_ID=$(docker images | grep "$(tested_image_name $TARGET)" | awk '{print $3}')
-if [[ -n $TESTED_ID ]] ; then
+TESTED_ID=$(docker images | grep "$(basic_image_name $TARGET)\s*tested" | awk '{print $3}')
+if [ $TESTED_ID ] ; then
 	docker rmi -f "$TESTED_ID"
 fi
 
